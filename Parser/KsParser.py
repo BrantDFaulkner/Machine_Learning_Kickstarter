c111055f486c63950ec6419fd7c77f3f0fac4478
BrantDFaulkner <BrantDFaulkner@gmail.com>
2018-03-26 19:48:26 -0500

    Cleaning house

--- Parser/KsParser.py
+++ Parser/KsParser.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 import os.path
 import pandas as pd
 from bs4 import BeautifulSoup
-import re as re
+import re
 import json
 
 
@@ -25,40 +25,26 @@ def gen_connection(path):
     conn.text_factory = str
     return conn
 
-# script = soup.find('script', text=re.compile('window\.blog\.data'))
-# json_text = re.search(r'^\s*window\.blog\.data\s*=\s*({.*?})\s*;\s*$',
-                    #   script.string, flags=re.DOTALL | re.MULTILINE).group(1)
-
 def json_text(soup):
-    # json_str = soup.find('script', text=re.complile('window\.current_project'))
-    # json_text = script
-    # json_str = str(soup.find(text=re.compile(r'window.current_project')))
     script = soup.find("script", string=re.compile(r'window.current_project'))
     json_str = script.string
     json_str = re.search('window\.current_project.*', json_str).group(0)
     json_str = json_str[26:-2]
     json_str = json_str.replace('&quot;', '"')
     json_str = json_str.replace(r'\\', '\\')
-
-    # my_list = "".join(list(json_str)[1900:2100])
-    # print my_list
-
-
-    json_str = json.loads(json_str)
-    # print json.dumps(json_str, indent=4, sort_keys=True)
-
-    # json_str = re.search('"{.*', json_str).group(0)
-
-    # json_str = json_str.replace('&#39;', "'")
-    # json_str = json_str.replace(r'\\"', "'")
-    # json_str = json.loads(json_str)
+    json_load = json.loads(json_str)
     return json_str
 
-def get_text(soup):
+def print_json_segment(json_str, begin_range, end_range):
+    segment = "".join(list(json_str)[begin_range:end_range])
+    return segment
+
+def pretty_print_json(json_str):
+    print json.dumps(json_str, indent=4, sort_keys=True)
+
+def get_text(soup):#MOVE TO PARSER
     div = soup.find("div", {"class": "full-description"})
     return div
-
-
 
 def get_soup():
     conn = gen_connection(gen_path())
@@ -68,14 +54,9 @@ def get_soup():
     for campaign in data:
         soup = BeautifulSoup(campaign[0], 'html.parser')
         div = get_text(soup)
-        print div.text
-        # soup = BeautifulSoup(campaign[0], 'lxml')
         x = json_text(soup)
         # print x["backers_count"]
     c.close()
     conn.close()
-
-
-
 
 get_soup()
